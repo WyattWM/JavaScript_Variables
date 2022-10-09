b468b54907af3c01e71a9f5b4570284ceaa36eb5
wyattwm <wyattwmwm@gmail.com>
2022-10-09 16:09:40 -0600

    variables

--- app.js
+++ app.js
@@ -3,8 +3,45 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 //
+const firstName = "Wyatt";
+let lastName = "Murdoch";
+var age = 23;
 
-// Exercise 2
+// Exercise 2 //
+lastName = "Murdoch";
+age = 23;
 
-// Exercise 3
+
+// Exercise 3 //
+let language = "JavaScript"
+let createdYear = 1999;
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}`);
+
+
+// Exercise 4 //
+let product = "T-shirt";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+
+console.log(
+  `${product} in size ${selectedSize} is $${price} with ${stock} in stock.`
+);
+
+// Exercise 5 //
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(
+  `${title} by ${author} is ${pageCount} pages long. Your last bookmark was page ${bookmark}.`
+)
