b2e81a6f1311a55ebb096231d1dc9139f4bc4423
wyattwm <wyattwmwm@gmail.com>
2024-02-15 15:40:20 -0700

    new file

---
+++ apps.js
@@ -0,0 +1,31 @@
+// Exercise 1 //
+
+const firstName = "Wyatt";
+let lastName = "Murdoch";
+var age = 24;
+
+
+// Exercise 3 //
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+
+// Exercise 4 //
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15
+let stock = 0;
+let inStock = flase;
+let selectedSize = "M";
+
+
+// Exercise 5 //
+
+let title = "Name of the Wind";
+let author = "Patrick RothFuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
